c78e44b78b0bc9ffda62697e0fdd108ee09c0592
tomasz nowak <tomasz.nowak.kmasz@gmail.com>
2018-06-09 10:07:42 +0200

    pierwsza działajaca applikacja

--- __init__.py
+++ __init__.py
@@ -12,7 +12,23 @@ import datetime
 import time
 import configparser
 
+###CONFIG PART
+config = configparser.ConfigParser()
+if config.read('config/config.conf'):
+    ##DEV
+    server_type = config['DEFAULT']['servertype']
+elif config.read('/var/www/FlaskApp/FlaskApp/config/config.conf'):
+    ## PROD
+    server_type = config['DEFAULT']['servertype']
+else:
+    pass ## TODO dorobić obsługę błędu
 
+db_file = config[server_type]['db_source'] # path to file
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+        html.H2('test')
+])
 
 
 ####  PROD ####
