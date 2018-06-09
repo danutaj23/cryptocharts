71539b3127f810fdeeec8046b7142011e3468dc9
tomasz nowak <tomasz.nowak.kmasz@gmail.com>
2018-06-09 14:15:42 +0200

    dodanie obslugi bledow

--- __init__.py
+++ __init__.py
@@ -21,43 +21,53 @@ elif config.read('/var/www/FlaskApp/FlaskApp/config/config.conf'):
     ## PROD
     server_type = config['DEFAULT']['servertype']
 else:
-    pass ## TODO dorobić obsługę błędu
+    with open('errors.txt', 'a') as error_log:
+        error_log.write(str(datetime.datetime.fromtimestamp(time.time())) + ': ' + "Can't open config.conf file!")
+        error_log.write('\n')
+        error_log.close()
 
-db_file = config[server_type]['db_source'] # path to file
+db_file = config[server_type]['db_source']  # path to file
 app = dash.Dash(__name__)
 
-conndb = sqlite3.connect(db_file)
-test = pd.read_sql('SELECT * FROM bitcoin ORDER BY last_updated DESC', conndb)
-conndb.close()
+#conndb = sqlite3.connect(db_file)
+#test = pd.read_sql('SELECT * FROM bitcoin ORDER BY last_updated DESC', conndb)
+#conndb.close()
 
 app.layout = html.Div([
     html.H2('Live Bitcoin Price',
             style={'textAlign': 'center', 'color': '#354B5E'}),
 
     dcc.Graph(id='live-graph', animate=True),
-    dcc.Interval(id='graph-update', interval=1*1000)
+    dcc.Interval(id='graph-update', interval=2*1000)
 ])
 
 
 @app.callback(Output('live-graph', 'figure'),
               events=[Event('graph-update', 'interval')])
 def update_graph_scatter():
-    conndb = sqlite3.connect(db_file)
-    query = 'SELECT * FROM bitcoin ORDER BY last_updated DESC'
-    all_currencies_data = pd.read_sql(query, conndb)
-    all_currencies_data.sort_values('last_updated', inplace=True)   # sortowanie wg czasu
-    all_currencies_data['date'] = pd.to_datetime(all_currencies_data['last_updated'], unit='s', utc=True)  # zmiana timestampa na czas
-    all_currencies_data.set_index('date', inplace=True)
-    X = all_currencies_data.index[-100:]
-    Y = all_currencies_data.price_usd.values[-100:]
-    data = plotly.graph_objs.Scatter(
-        x=X,
-        y=Y,
-        name='Scatter',
-        mode='lines+markers'
-    )
-    return{'data': [data], 'layout': go.Layout(xaxis={'range': [min(X), max(X)], 'title': 'Time'},
-                                               yaxis={'range': [min(Y), max(Y)], 'title': 'Value in USD'}), }
+    try:
+        conndb = sqlite3.connect(db_file)
+        query = 'SELECT * FROM bitcoin ORDER BY last_updated DESC'
+        all_currencies_data = pd.read_sql(query, conndb)
+        all_currencies_data.sort_values('last_updated', inplace=True)   # sortowanie wg czasu
+        all_currencies_data['date'] = pd.to_datetime(all_currencies_data['last_updated'], unit='s', utc=True)  # zmiana timestampa na czas
+        all_currencies_data.set_index('date', inplace=True)
+        X = all_currencies_data.index[-100:]
+        Y = all_currencies_data.price_usd.values[-100:]
+        data = plotly.graph_objs.Scatter(
+            x=X,
+            y=Y,
+            name='Scatter',
+            mode='lines+markers'
+        )
+        return{'data': [data], 'layout': go.Layout(xaxis={'range': [min(X), max(X)], 'title': 'Time'},
+                                                   yaxis={'range': [min(Y), max(Y)], 'title': 'Value in USD'}), }
+    except Exception as e:
+        with open('errors.txt', 'a') as error_log:
+            error_log.write(str(datetime.datetime.fromtimestamp(time.time())) + ': ' + str(e))
+            error_log.write('\n')
+            error_log.close()
+
 
 
 #### PROD ####
