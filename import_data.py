4c175b4da4f49a6d87c716b0a24b832c6b48e412
danutaj23 <danoota@o2.pl>
2018-06-16 10:26:28 +0200

    refaktoryzacja w import_data.py

--- import_data.py
+++ import_data.py
@@ -23,11 +23,15 @@ db_file = config[server_type]['db_source'] #path to db file
 db_nbp_file = config[server_type]['db_nbp_source'] #path to nbp db file
 db_log = config[server_type]['log_db'] #path to db log file
 sleep_time = int(config['DEFAULT']['sleep_time']) #czas pomiędzy odczytami danych
-logging.basicConfig(filename=db_log,level=logging.DEBUG, format='%(asctime)s :: %(levelname)s >> %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename=db_log,
+                    level=logging.DEBUG,
+                    format='%(asctime)s :: %(levelname)s >> %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 db_data_types = {'24h_volume_usd': 'REAL',
                  'available_supply': 'REAL',
-                 'id': 'TEXT', 'last_updated': 'REAL',
+                 'id': 'TEXT',
+                 'last_updated': 'REAL',
                  'market_cap_usd': 'REAL',
                  'max_supply': 'REAL',
                  'name': 'TEXT',
@@ -56,7 +60,7 @@ def main():
             data = json.loads(data)
             data = pd.DataFrame(data)
             continiousreading(data) #nieskończona pętla, która zczytuje dane
-    ##PROD################################################### umieszczone w cron
+    ##PROD################################################# umieszczone w cron
     elif server_type == 'PROD':
         data = request.urlopen(dataLink)
         data = data.read().decode("utf-8")
@@ -64,12 +68,12 @@ def main():
         data = pd.DataFrame(data)
         continiousreading(data)
     else:
-        pass  # TODO dodać obsługę błedu i logowanie
+        pass  # TODO dodać obsługę błędu i logowanie
 
 
 def timestamp2time(ts):
-    #jeśli ts nie jest INT to konwertuj na INT
-    #TODO dodać obsługe błędu i logowanie
+    # jeśli to nie jest INT to konwertuj na INT
+    # TODO dodać obsługe błędu i logowanie
     if not isinstance(ts, int):
         ts = int(ts)
     _time = datetime.datetime.fromtimestamp(ts)  #konwersja z linux timestamp na datę
@@ -85,15 +89,15 @@ def replace_str(str_to_fix):
 
 
 def continiousreading(data):
-    #TODO dodać sprawdznie czy dane się zmieniły, jeśli nie to nic nie robić
+    # TODO dodać sprawdznie czy dane się zmieniły, jeśli nie to nic nie robić
     conn = sqlite3.connect(db_file)
-    #datafile = open(db_log, 'a')
+    # datafile = open(db_log, 'a')
     kolumna_danych = data.loc[:, 'id']   #bierzemy z DataFrame tylko kolumnę z 'id'
-    #datafile.write(str(timestamp2time(time.time()))+ ' : Zaczynam zapis danych')
+    # datafile.write(str(timestamp2time(time.time()))+ ' : Zaczynam zapis danych')
     logging.info('Zaczynam zapis danych')
     for k in range(0, len(kolumna_danych)):  #przechodzimy przez wszystkie zczytane dane -k jest od 0 do długości zmiennej: kolumna_danych
         str_data = data[data['id'] == kolumna_danych[k]]    #czytamy wiersz w którym 'id' równe jest wartości kalumna danych[k]
-        table_name = str(kolumna_danych[k]) #zminna przechowująca nazwę tabeli
+        table_name = str(kolumna_danych[k]) #zmienna przechowująca nazwę tabeli
         table_name = replace_str(table_name)
         #print(table_name)
         # c.execute('CREATE TABLE IF NOT EXISTS table_name("24h_volume_usd" REAL, available_supply REAL, id TEXT, last_updated REAL, market_cap_usd REAL, max_supply REAL, name TEXT, percent_change_1h REAL, percent_change_24h REAL, percent_change_7d REAL, price_btc REAL, price_usd REAL, rank INTEGER, symbol TEXT, total_supply REAL)')
